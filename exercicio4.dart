3e814977bb65da450014d1aa2e23e2e1407e8f25
Erik Marques <erikmarques@sisconsultoria.com.br>
2022-03-05 10:15:45 -0300

    feat: exercise 4

---
+++ exercicio4.dart
@@ -0,0 +1,8 @@
+//Entrar via teclado com o valor de uma temperatura em graus Celsius,
+//calcular e exibir sua temperatura equivalente em Fahrenheit.
+
+void main() {
+  double celsius = 2.0;
+  double fahrenheit = celsius * 1.8 + 32;
+  print('Fahrenheit $fahrenheit');
+}
