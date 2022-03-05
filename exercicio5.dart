55b77861b717e63ada0749fd587644afde8778c9
Erik Marques <erikmarques@sisconsultoria.com.br>
2022-03-05 10:17:13 -0300

    feat: exercise 5

---
+++ exercicio5.dart
@@ -0,0 +1,8 @@
+// Entrar via teclado com o valor da cotação do dólar e uma certa
+//quantidade de dólares. Calcular e exibir o valor correspondente em Reais (R$).
+
+void main() {
+  double dolar = 2.0;
+  double reais = dolar * 5.06;
+  print('Reais: $reais');
+}
