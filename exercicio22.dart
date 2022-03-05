358ac2cf9b3b77377a940db1265fef12d35cc615
Erik Marques <erikmarques@sisconsultoria.com.br>
2022-03-05 11:22:44 -0300

    vou continuar ainda kkkk

---
+++ exercicio22.dart
@@ -0,0 +1,4 @@
+// Criar um mini sistema bancário com as classes: Conta, Conta Corrente, 
+//Conta Poupança e Cliente. Cada classe deve ter atributos específicos. 
+//No sistema devemos ter pelo menos 3 funcionalidades: Saque, Depósito e
+// Transferência.
