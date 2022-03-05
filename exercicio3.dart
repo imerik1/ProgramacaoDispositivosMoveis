79ec901ff92a33e23c7b5bf679644bae8e1a44b7
Erik Marques <erikmarques@sisconsultoria.com.br>
2022-03-05 10:14:21 -0300

    feat: exercise 3

---
+++ exercicio3.dart
@@ -0,0 +1,11 @@
+// Calcular e exibir a média aritmética de
+//quatro valores quaisquer que serão digitados.
+
+void main() {
+  double n1 = 2.0;
+  double n2 = 5.0;
+  double n3 = 1.0;
+  double n4 = 6.0;
+  double media = (n1 + n2 + n3 + n4) / 4;
+  print('A media aritmetica $media');
+}
