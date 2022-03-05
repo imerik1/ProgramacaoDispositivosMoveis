3a36d9b964e52d22d3621931cbecfdf8f2b6e855
Erik Marques <erikmarques@sisconsultoria.com.br>
2022-03-05 10:12:33 -0300

    feat: exercise 2

--- exercicio1.dart
+++ exercicio1.dart
@@ -1,6 +1,7 @@
-void main {
-    double n1 = 2.0;
-    double n2 = 5.0;
-    double media = (n1/n2)/2;
-    print('A media aritmetica $media')
+// Calcular média artimetica.
+void main() {
+  double n1 = 2.0;
+  double n2 = 5.0;
+  double media = (n1 / n2) / 2;
+  print('A media aritmetica $media');
 }
---
+++ exercicio2.dart
@@ -0,0 +1,8 @@
+// Entrar via teclado com a base e a altura de um retângulo,
+//calcular e exibir sua área.
+void main() {
+  double base = 5.0;
+  double altura = 2.0;
+  double area = (base * altura) / 2;
+  print('A área do retangulo é $area');
+}
