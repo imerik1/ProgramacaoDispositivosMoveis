44cda36b2099ec9427ace186bf992f4fa2d92b30
Erik Marques <erikmarques@sisconsultoria.com.br>
2022-03-05 10:50:49 -0300

    feat: exercise 15

---
+++ exercicio15.dart
@@ -0,0 +1,13 @@
+// Exibir os vinte primeiros valores da série de
+//Bergamaschi. A série: 1, 1, 1, 3, 5, 9, 17, ...
+
+int berg(int n) {
+  return (n <= 3) ? 1 : berg(n - 1) + berg(n - 2) + berg(n - 3);
+}
+
+void main() {
+  double f = 20;
+  for (int i = 1; i <= f; i++) {
+    print(berg(i));
+  }
+}
