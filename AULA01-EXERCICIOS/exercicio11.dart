40b10f29240a66cce349fe111515c03f27dced17
Erik Marques <65323066+imerik1@users.noreply.github.com>
2022-03-11 20:45:01 -0300

    Update exercicio11.dart

--- AULA01-EXERCICIOS/exercicio11.dart
+++ AULA01-EXERCICIOS/exercicio11.dart
@@ -11,5 +11,9 @@ void main() {
   final double v0 = validarInputNumber("Digite a velocidade inicial", null);
   final double t = validarInputNumber("Digite o tempo de percurso", null);
   final double f = v0 + (a * t);
-  print('A velocidade final é de ${f}km/h');
+	if(f <= 40) return print("Velocidade muito lento.");
+	if(f >= 40 && f <= 60) return print("Velocidade permitida.");
+	if(f >= 60 && f <= 80) return print("Velocidade de cruzeiro.");
+	if(f >= 80 && f <= 120) return print("Velocidade rápido.");
+	if(f > 120) return print("Velocidade muito rápido.");
 }
