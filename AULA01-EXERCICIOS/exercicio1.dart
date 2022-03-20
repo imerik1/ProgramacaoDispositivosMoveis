2a0895e06a6e66186aef0ba553caa023e7f93cf0
Erik Marques <santanaerik1106@gmail.com>
2022-03-19 21:09:33 -0300

    innitial aula 05

--- AULA01-EXERCICIOS/exercicio1.dart
+++
@@ -1,9 +0,0 @@
-// Calcular média artimetica.
-import './utils/validarInputNumber.dart';
-
-void main() {
-  double n1 = validarInputNumber("Digite o primeiro valor", null);
-  double n2 = validarInputNumber("Digite o segundo valor", null);
-  final double media = (n1 + n2) / 2;
-  print('A media aritmetica $media');
-}
--- AULA02-APPHELLO/android/app/src/main/kotlin/com/example/app_hello/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app_hello
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
